46e3cc4e3d08d14b00d43cda4e1327f7ddbd19fc
Francisco Edno <ednofco@gmail.com>
2018-08-11 14:26:24 -0300

    Configuração dos testes unitários

---
+++ src/AppBundle/Tests/Unit/Service/Importer/ImporterTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: edno
+ * Date: 11/08/18
+ * Time: 14:03
+ */
+
+namespace AppBundle\Tests\Unit\Service\Importer;
+
+use PHPUnit\Framework\TestCase;
+
+class ImporterTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function test_import_ValidPeopleXml_ShouldReturnACollectionOfPerson()
+    {
+        $this->assertTrue(true);
+    }
+}
