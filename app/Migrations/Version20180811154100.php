cba6aac66deae8c974136bd99d6d95ab27ce2c54
Francisco Edno <ednofco@gmail.com>
2018-08-11 12:44:00 -0300

    Adicionado tabela order

---
+++ app/Migrations/Version20180811154100.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180811154100 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('
+        CREATE TABLE `order` (
+            id BIGINT NOT NULL AUTO_INCREMENT,
+            person_id BIGINT NOT NULL,
+            shipping_address_id BIGINT NOT NULL,
+            CONSTRAINT order_PK PRIMARY KEY (id),
+            CONSTRAINT order_person_FK FOREIGN KEY (person_id) REFERENCES towel.person(id),
+            CONSTRAINT order_shipping_address_FK FOREIGN KEY (shipping_address_id) REFERENCES towel.shipping_address(id)
+        )
+        ENGINE=InnoDB
+        DEFAULT CHARSET=utf8
+        COLLATE=utf8_general_ci;
+        ');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql('DROP TABLE `order`;');
+    }
+}
