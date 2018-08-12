fe2535da7958d34b56b479b3b2c158140a9648b3
Francisco Edno <ednofco@gmail.com>
2018-08-12 17:41:57 -0300

    Injetado novas dependências do orderimporter

--- src/AppBundle/Service/Importer/XmlImporter/OrderImporter.php
+++ src/AppBundle/Service/Importer/XmlImporter/OrderImporter.php
@@ -13,6 +13,9 @@ use AppBundle\Entity\Order;
 use AppBundle\Entity\Person;
 use AppBundle\Entity\ShippingAddress;
 use AppBundle\Repository\Contract\OrderRepository;
+use AppBundle\Repository\Contract\PersonRepository;
+use AppBundle\Service\Extractor\Dto\ShipOrder;
+use AppBundle\Service\Extractor\Extractor;
 use AppBundle\Service\Importer\XmlImporter;
 
 class OrderImporter extends XmlImporter
@@ -20,11 +23,24 @@ class OrderImporter extends XmlImporter
     /**
      * @var OrderRepository
      */
-    private $repository;
+    private $orderRepository;
+    /**
+     * @var Extractor
+     */
+    private $extractor;
+    /**
+     * @var PersonRepository
+     */
+    private $personRepository;
 
-    public function __construct(OrderRepository $repository)
-    {
-        $this->repository = $repository;
+    public function __construct(
+        Extractor $extractor,
+        PersonRepository $personRepository,
+        OrderRepository $orderRepository
+    ) {
+        $this->orderRepository = $orderRepository;
+        $this->extractor = $extractor;
+        $this->personRepository = $personRepository;
     }
 
     /**
@@ -32,8 +48,18 @@ class OrderImporter extends XmlImporter
      */
     public function import(string $source)
     {
+        $shipOrders = $this->extractor->extractShipOrders($source);
+        array_walk($shipOrders, [$this, 'importOrder']);
+    }
+
+    /**
+     * @param ShipOrder $shipOrder
+     */
+    private function importOrder(ShipOrder $shipOrder)
+    {
         $person = new Person(1, 'Name 1');
         $addr = new ShippingAddress();
-        $this->repository->save(new Order(1, $person, $addr));
+        $order = new Order(1, $person, $addr);
+        $this->orderRepository->save($order);
     }
 }
--- src/AppBundle/Tests/Unit/Service/Importer/XmlImporter/OrderImporterTest.php
+++ src/AppBundle/Tests/Unit/Service/Importer/XmlImporter/OrderImporterTest.php
@@ -9,10 +9,16 @@
 namespace AppBundle\Tests\Unit\Service\Importer\XmlImporter;
 
 
+use AppBundle\Entity\Person;
+use AppBundle\Repository\Contract\PersonRepository;
+use AppBundle\Service\Extractor\Dto\Item;
+use AppBundle\Service\Extractor\Dto\ShipOrder;
+use AppBundle\Service\Extractor\Dto\ShipTo;
 use AppBundle\Service\Extractor\Extractor;
 use AppBundle\Service\Importer\XmlImporter\OrderImporter;
 use AppBundle\Tests\Mock\InMemoryOrderRepository;
 use PHPUnit\Framework\TestCase;
+use \Mockery as m;
 
 class OrderImporterTest extends TestCase
 {
@@ -21,8 +27,36 @@ class OrderImporterTest extends TestCase
      */
     public function import_ValidXmlGiven_ShouldPersistTheSameOrderQuantity()
     {
+        $extractor = m::mock(Extractor::class)
+            ->shouldReceive('extractShipOrders')
+            ->andReturn([
+                (new ShipOrder())
+                    ->setOrderid(1)
+                    ->setOrderperson(1)
+                    ->setShipto(
+                        (new ShipTo())
+                            ->setName('Name 1')
+                            ->setAddress('Address 1')
+                            ->setCity('City 1')
+                            ->setCountry('Country 1')
+                    )
+                    ->setItems([
+                        (new Item())
+                            ->setTitle('Title 1')
+                            ->setNote('Note 1')
+                            ->setQuantity(745)
+                            ->setPrice(123.45)
+                    ])
+            ])
+            ->getMock()
+        ;
+        $personRepository = m::mock(PersonRepository::class)
+            ->shouldReceive('find')
+            ->andReturn(new Person(1, 'Name 1'))
+            ->getMock()
+        ;
         $orderRepository = new InMemoryOrderRepository();
-        $orderImporter = new OrderImporter($orderRepository);
+        $orderImporter = new OrderImporter($extractor, $personRepository, $orderRepository);
         $orderImporter->import(file_get_contents(__DIR__ . '/../../../../Resources/shiporders_simple.xml'));
         $orders = $orderRepository->findAll();
         $this->assertCount(1, $orders);
