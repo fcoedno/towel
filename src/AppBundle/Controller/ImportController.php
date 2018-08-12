6722f8541e510a85614de31627fdf30204aba1a4
Francisco Edno <ednofco@gmail.com>
2018-08-12 12:49:05 -0300

    Adicionado rota para processar o xml

--- src/AppBundle/Controller/ImportController.php
+++ src/AppBundle/Controller/ImportController.php
@@ -11,14 +11,27 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ImportController extends Controller
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="index", methods={"GET"})
      */
     public function indexAction()
     {
         return $this->render('index.twig');
+    }
+
+    /**
+     * @Route("/proccess", methods={"POST"}, name="proccess_xml")
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function proccessXmlAction(Request $request)
+    {
+        $this->addFlash('success', 'XML files were proccessed!');
+        return $this->redirectToRoute('index');
     }
 }
