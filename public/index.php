3bace1263165c0c19991d290fcc91910977bcf3d
Francisco Edno <francisco.edno@s2it.com.br>
2018-11-18 01:16:18 -0200

    Refactored the root namespace to Towel

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Infra\Framework\Kernel;
+use Towel\Infra\Framework\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
--- src/Infra/Framework/Kernel.php
+++ src/Infra/Framework/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Infra\Framework;
+namespace Towel\Infra\Framework;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
